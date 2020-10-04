4c60ea172bec9cc268bd93601ebef5b1f5d5ad22
Fumikazu Fujiwara <github@ze.gs>
2020-10-04 22:00:25 +0900

    add minimize

--- lib/minimize_change_model.dart
+++ lib/minimize_change_model.dart
@@ -16,12 +16,13 @@ enum Bill {
 
 class MinimizeChangeModel extends ChangeNotifier {
   List<int> _bills = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  List<int> _pays = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   int _billing = 0;
   MinimizeChangeModel() {
     this.clear();
   }
   void clear() {
-    Bill.values.forEach((b) => this._bills[b.index] = 0);
+    Bill.values.forEach((b) => this._bills[b.index] = this._pays[b.index] = 0);
     this._billing = 0;
     notifyListeners();
   }
@@ -31,8 +32,12 @@ class MinimizeChangeModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  int number(Bill b) {
+  int numberOfBills(Bill b) {
     return this._bills[b.index];
+  }
+
+  int numberOfPays(Bill b) {
+    return this._pays[b.index];
   }
 
   int sum() {
@@ -43,6 +48,82 @@ class MinimizeChangeModel extends ChangeNotifier {
           : this._bills[b.index] * pow(10, (b.index + 1) / 2) / 2;
     });
     return sum.round();
+  }
+
+  void minimize() {
+    int rest = this._billing;
+    // one
+    if (this._bills[Bill.one.index] >= rest % 10) {
+      this._pays[Bill.one.index] = rest % 10;
+      this._bills[Bill.one.index] -= rest % 10;
+    } else if (rest % 10 < 5) {
+      if (this._bills[Bill.five.index] > 0) {
+        this._pays[Bill.five.index]++;
+        this._bills[Bill.five.index]--;
+      }
+    } else {
+      if (this._bills[Bill.one.index] >= (rest % 10 - 5)) {
+        this._pays[Bill.one.index] = rest % 10 - 5;
+        this._bills[Bill.one.index] -= rest % 10 - 5;
+      }
+    }
+    rest -= rest % 10;
+
+    // ten
+    if (this._bills[Bill.ten.index] >= ((rest % 100) / 10).round()) {
+      this._pays[Bill.ten.index] = ((rest % 100) / 10).round();
+      this._bills[Bill.ten.index] -= ((rest % 100) / 10).round();
+    } else if (((rest % 100) / 10).round() < 5) {
+      if (this._bills[Bill.fifty.index] > 0) {
+        this._pays[Bill.fifty.index]++;
+        this._bills[Bill.fifty.index]--;
+      }
+    } else {
+      if (this._bills[Bill.ten.index] >= (((rest % 100) / 10).round() - 5)) {
+        this._pays[Bill.ten.index] = ((rest % 100) / 10).round() - 5;
+        this._bills[Bill.ten.index] -= ((rest % 100) / 10).round() - 5;
+      }
+    }
+    rest -= rest % 100;
+
+    // hundred
+    if (this._bills[Bill.oneHundred.index] >= ((rest % 1000) / 100).round()) {
+      this._pays[Bill.oneHundred.index] = ((rest % 1000) / 100).round();
+      this._bills[Bill.oneHundred.index] -= ((rest % 1000) / 100).round();
+    } else if (((rest % 1000) / 100).round() < 5) {
+      if (this._bills[Bill.fiveHundreds.index] > 0) {
+        this._pays[Bill.fiveHundreds.index]++;
+        this._bills[Bill.fiveHundreds.index]--;
+      }
+    } else {
+      if (this._bills[Bill.oneHundred.index] >=
+          (((rest % 1000) / 100).round() - 5)) {
+        this._pays[Bill.oneHundred.index] = ((rest % 1000) / 100).round() - 5;
+        this._bills[Bill.oneHundred.index] -= ((rest % 1000) / 100).round() - 5;
+      }
+    }
+    rest -= rest % 1000;
+
+    // thousand
+    if (this._bills[Bill.oneThousand.index] >=
+        ((rest % 10000) / 1000).round()) {
+      this._pays[Bill.oneThousand.index] = ((rest % 10000) / 1000).round();
+      this._bills[Bill.oneThousand.index] -= ((rest % 10000) / 1000).round();
+    } else if (((rest % 10000) / 1000).round() < 5) {
+      if (this._bills[Bill.fiveThousands.index] > 0) {
+        this._pays[Bill.fiveThousands.index]++;
+        this._bills[Bill.fiveThousands.index]--;
+      }
+    } else {
+      if (this._bills[Bill.oneThousand.index] >=
+          (((rest % 10000) / 1000).round() - 5)) {
+        this._pays[Bill.oneThousand.index] =
+            ((rest % 10000) / 1000).round() - 5;
+        this._bills[Bill.oneThousand.index] -=
+            ((rest % 10000) / 1000).round() - 5;
+      }
+    }
+    rest -= rest % 10000;
   }
 
   bool canPay() => this._billing <= this.sum();
--- test/minimize_change_model_test.dart
+++ test/minimize_change_model_test.dart
@@ -26,15 +26,15 @@ void main() {
       expect(c.sum(), 16665);
       c.increment(Bill.one, 1);
       expect(c.sum(), 16666);
-      expect(c.number(Bill.tenThousands), 1);
-      expect(c.number(Bill.fiveThousands), 1);
-      expect(c.number(Bill.oneThousand), 1);
-      expect(c.number(Bill.fiveHundreds), 1);
-      expect(c.number(Bill.oneHundred), 1);
-      expect(c.number(Bill.fifty), 1);
-      expect(c.number(Bill.ten), 1);
-      expect(c.number(Bill.five), 1);
-      expect(c.number(Bill.one), 1);
+      expect(c.numberOfBills(Bill.tenThousands), 1);
+      expect(c.numberOfBills(Bill.fiveThousands), 1);
+      expect(c.numberOfBills(Bill.oneThousand), 1);
+      expect(c.numberOfBills(Bill.fiveHundreds), 1);
+      expect(c.numberOfBills(Bill.oneHundred), 1);
+      expect(c.numberOfBills(Bill.fifty), 1);
+      expect(c.numberOfBills(Bill.ten), 1);
+      expect(c.numberOfBills(Bill.five), 1);
+      expect(c.numberOfBills(Bill.one), 1);
     });
     test("clear", () {
       c.increment(Bill.tenThousands, 1);
@@ -70,6 +70,165 @@ void main() {
       expect(c.canPay(), true);
       c.increment(Bill.one, 1);
       expect(c.canPay(), true);
+    });
+
+    test("minimize", () {
+      //one
+      c.billing = 14;
+      c.increment(Bill.one, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.one), 3);
+      expect(c.numberOfPays(Bill.one), 0);
+      c.clear();
+
+      c.billing = 14;
+      c.increment(Bill.one, 4);
+      c.minimize();
+      expect(c.numberOfBills(Bill.one), 0);
+      expect(c.numberOfPays(Bill.one), 4);
+      c.clear();
+
+      c.billing = 14;
+      c.increment(Bill.one, 3);
+      c.increment(Bill.five, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.one), 3);
+      expect(c.numberOfPays(Bill.one), 0);
+      expect(c.numberOfBills(Bill.five), 2);
+      expect(c.numberOfPays(Bill.five), 1);
+      c.clear();
+
+      c.billing = 15;
+      c.increment(Bill.one, 6);
+      c.minimize();
+      expect(c.numberOfBills(Bill.one), 1);
+      expect(c.numberOfPays(Bill.one), 5);
+      c.clear();
+
+      c.billing = 16;
+      c.increment(Bill.one, 1);
+      c.minimize();
+      expect(c.numberOfBills(Bill.one), 0);
+      expect(c.numberOfPays(Bill.one), 1);
+      c.clear();
+
+      //ten
+      c.billing = 140;
+      c.increment(Bill.ten, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.ten), 3);
+      expect(c.numberOfPays(Bill.ten), 0);
+      c.clear();
+
+      c.billing = 140;
+      c.increment(Bill.ten, 4);
+      c.minimize();
+      expect(c.numberOfBills(Bill.ten), 0);
+      expect(c.numberOfPays(Bill.ten), 4);
+      c.clear();
+
+      c.billing = 140;
+      c.increment(Bill.ten, 3);
+      c.increment(Bill.fifty, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.ten), 3);
+      expect(c.numberOfPays(Bill.ten), 0);
+      expect(c.numberOfBills(Bill.fifty), 2);
+      expect(c.numberOfPays(Bill.fifty), 1);
+      c.clear();
+
+      c.billing = 150;
+      c.increment(Bill.ten, 6);
+      c.minimize();
+      expect(c.numberOfBills(Bill.ten), 1);
+      expect(c.numberOfPays(Bill.ten), 5);
+      c.clear();
+
+      c.billing = 160;
+      c.increment(Bill.ten, 1);
+      c.minimize();
+      expect(c.numberOfBills(Bill.ten), 0);
+      expect(c.numberOfPays(Bill.ten), 1);
+      c.clear();
+
+      //oneHundred
+      c.billing = 1400;
+      c.increment(Bill.oneHundred, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneHundred), 3);
+      expect(c.numberOfPays(Bill.oneHundred), 0);
+      c.clear();
+
+      c.billing = 1400;
+      c.increment(Bill.oneHundred, 4);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneHundred), 0);
+      expect(c.numberOfPays(Bill.oneHundred), 4);
+      c.clear();
+
+      c.billing = 1400;
+      c.increment(Bill.oneHundred, 3);
+      c.increment(Bill.fiveHundreds, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneHundred), 3);
+      expect(c.numberOfPays(Bill.oneHundred), 0);
+      expect(c.numberOfBills(Bill.fiveHundreds), 2);
+      expect(c.numberOfPays(Bill.fiveHundreds), 1);
+      c.clear();
+
+      c.billing = 1500;
+      c.increment(Bill.oneHundred, 6);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneHundred), 1);
+      expect(c.numberOfPays(Bill.oneHundred), 5);
+      c.clear();
+
+      c.billing = 1600;
+      c.increment(Bill.oneHundred, 1);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneHundred), 0);
+      expect(c.numberOfPays(Bill.oneHundred), 1);
+      c.clear();
+
+
+      //oneThousand
+      c.billing = 14000;
+      c.increment(Bill.oneThousand, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneThousand), 3);
+      expect(c.numberOfPays(Bill.oneThousand), 0);
+      c.clear();
+
+      c.billing = 14000;
+      c.increment(Bill.oneThousand, 4);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneThousand), 0);
+      expect(c.numberOfPays(Bill.oneThousand), 4);
+      c.clear();
+
+      c.billing = 14000;
+      c.increment(Bill.oneThousand, 3);
+      c.increment(Bill.fiveThousands, 3);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneThousand), 3);
+      expect(c.numberOfPays(Bill.oneThousand), 0);
+      expect(c.numberOfBills(Bill.fiveThousands), 2);
+      expect(c.numberOfPays(Bill.fiveThousands), 1);
+      c.clear();
+
+      c.billing = 15000;
+      c.increment(Bill.oneThousand, 6);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneThousand), 1);
+      expect(c.numberOfPays(Bill.oneThousand), 5);
+      c.clear();
+
+      c.billing = 16000;
+      c.increment(Bill.oneThousand, 1);
+      c.minimize();
+      expect(c.numberOfBills(Bill.oneThousand), 0);
+      expect(c.numberOfPays(Bill.oneThousand), 1);
+      c.clear();
     });
   });
 }

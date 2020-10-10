323ba77a3bfb1c64c5fcad6b7f02277ba6cedf73
Fumikazu Fujiwara | Freddie <github@ze.gs>
2020-10-10 14:51:13 +0900

    Update input.dart

--- lib/input.dart
+++ lib/input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jpy_minimize_your_change/minimize_change_model.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 class Input extends StatelessWidget {
   // This widget is the root of your application.
